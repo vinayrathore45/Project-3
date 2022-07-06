a5227f29db2f6dd5b2a242fed1daf8b14e826b36
mantoopandey <vikaspandey42@gmail.com>
2022-07-06 15:52:00 +0530

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,8 @@
 const  bookModel = require("../models/bookModel");
 const  userModel = require("../models/userModel")
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 
 let createUser = async function (req,res){
@@ -14,3 +16,144 @@ let createUser = async function (req,res){
         res.status(500).send({status:false, message:error.message})
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const validPwd = (Password) => {
+    if (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/.test(Password)) {
+      return false
+    } else {
+      return true;
+    }
+  };
+
+const validEmail = (Email) => {
+    if (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(Email)) {
+      return false
+    } else {
+      return true;
+    }
+};
+  
+const  isValidBody = (object) => {
+    if (Object.keys(object).length > 0) {
+      return false
+    } else {
+      return true;
+    }
+}
+
+
+const userLogin = async function (req, res) {
+    try {
+        let data = req.body;
+        
+        if(isValidBody(data)){
+            return res.status(400).send({status:false,message: "enter user details"});  
+        }
+        if(!data.Email){
+            return res.status(400).send({status:false,message: "email id is required "});
+        }
+        if (!data.Password){
+            return res.status(400).send({status:false,message: "passward is required "});
+        }
+        
+    if (validEmail(data.Email)) {
+        return res.status(400).send({ status: false, message: "Enter a valid email-id" });
+      }
+      
+    if (validPwd(data.password)) {
+        return res.status(400).send({ status: false, message: "Enter a valid password" });
+      }
+      const checkValidUser = await User.findOne({ email: data.email });
+
+    if (!checkValidUser) {
+      return res.status(401).send({ status: false, message: "Email Id is not correct " });
+    }
+    
+    let checkPassword = await bcrypt.compare(data.password, checkValidUser.password);
+
+    if (!checkPassword) {
+      return res.status(401).send({ status: false, message: "Password is not correct" });
+    }
+    
+    let checkpassword = await bcrypt.compare(data.password, checkValidUser.password);
+
+    if (!checkpassword) {
+      return res.status(401).send({ status: false, message: "Password is not correct" });
+    }
+
+    let token = jwt.sign({ userId: checkValidUser._id }, "Books-Management", {expiresIn: '1d'});
+
+    
+
+    res.setHeader('x-api-key', token);
+
+    res.status(200).send({ status: true, message: "Successfully Login", data: token });
+
+
+        
+    } catch (error) {
+        res.status(500).send({status: false,message: error.message});
+    }
+
+
+        
+    }
+    module.exports = { createUser, userLogin };
+
+
+
+
+
+
+

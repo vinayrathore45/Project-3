b76d374135b58cd0f37b03fb4a9d904b1811d882
SHRI2310 <shrikantajmire1298@gmail.com>
2022-07-07 21:40:32 +0530

    7july night

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,6 +2,20 @@ const userModel = require("../models/userModel");
 const bookModel = require("../models/bookModel");
 const mongoose = require("mongoose");
 
+const ObjectId = require('mongoose').Types.ObjectId;
+
+
+function isValidObjectId(id) {
+
+  if (ObjectId.isValid(id)) {
+      if ((String)(new ObjectId(id)) === id)
+          return true;
+      return false
+  }
+  return false
+}
+
+
 
 
 const isValid = function (value) {
@@ -14,11 +28,19 @@ const isValid = function (value) {
 
 }
 
+const isvalidRequest = function (requestBody) {
+  return Object.keys(requestBody).length > 0
+}
+
+
+
 const createBook = async function (req, res) {
     try{
       
       let data = req.body;
-      
+      if(!isvalidRequest(data)){
+        return res.status(400).send({status:false,message: "enter user details"});  
+      }
 
       if (!data.title) {
         return res.status(400).send({ status: false, message: "Please provide title" });
@@ -137,5 +159,41 @@ const createBook = async function (req, res) {
     }
 }
       
+////===========================//======================updateBook API==========================//====================////
+
+
+const updateBook = async function (req,res){
+ let bookId= req.params.bookId
+//  console.log(bookId)
+ if (bookId!= null) {
+  r = isValidObjectId(bookId)
+  // console.log(r)
+  if (r == false) { return res.status(400).send({ msg: "inavalid id format" }) }
+}
+
+ let body =req.body
+ if(!isvalidRequest(body)){
+  return res.status(400).send({status:false,message: "enter user details"});  
+ }
+
+  let title =body.title
+  if (!isValid(title)) return res.status(400).send({ status: false, message: "title is not correct." })
+
+let excerpt = body.excerpt
+let releaseDate= body.releaseDate
+let ISBN =body.ISBN
+ 
+ let checkUnique= await bookModel.find({$or:[{title:title},{ISBN:ISBN}]})
+ if(checkUnique.length!=0) return res.status(400).send({status:false,message:"already present in DB"})
+
+ let updateBook = await bookModel.findOneAndUpdate({_id:bookId,isDeleted:false},{$set:body},{new:true})
+ if(!updateBook) return res.status(404).send({status:false, message:"No such book present"})
+ return res.status(200).send({ status: true, message:"Success",data:updateBook  })
+
+
+}
+
+
     
-    module.exports.createBook=createBook
+    module.exports.createBook =createBook
+    module.exports.updateBook = updateBook
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const { updateBook } = require('../controllers/bookController');
 const { createUser, userLogin } = require('../controllers/userController');
-
+const MW = require("../middleware/auth")
 
 const router = express.Router();
 
 
 router.post('/register', createUser);
 router.post('/login', userLogin);
+router.put('/books/:bookId',MW.authentication,updateBook)
 
 
 module.exports = router
